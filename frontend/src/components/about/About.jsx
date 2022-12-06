40ecbd93d94d2c3f95c58fa267c8f57cb38a7818
Jhonathan <jhonathan@Jhonathans-Mac-mini.local>
2022-12-06 19:47:57 -0300

    about page

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,5 +1,27 @@
+import monster from '../../assets/about/monster.svg'
+import letter from '../../assets/about/letter.svg'
+
 export default function About(){
     return (
-        <></>
+        <div className="about__container">
+            <img src={monster} alt="pink monster showing your tongue with three eyes" className="about__image"/>
+            <p>Hey</p>
+            <p>Thank you for support our application.</p>
+            <p>Its purpouse is to be a seasonal themed app adapting in response for some holiday</p>
+            <p>We hope you enjoy</p>
+            <p>If you have something to say please feel free to let us know using the form below</p>
+            <div className="about-form__container">
+                <div style={{display: 'flex'}}>
+                <img src={letter} alt="letter with skull" className="about-form__icon"/>
+                <form style={{marginRight: '80px'}}>
+                    <input className="general-input"/>
+                    <input className="general-input"/>
+                    <textarea className="general-input" ></textarea>
+                </form>
+                </div>
+                <button style={{margin: '40px'}}>sudhushdhushdushd</button>
+            </div>
+
+        </div>
     )
 }
