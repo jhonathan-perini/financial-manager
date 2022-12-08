c0d1237bd8991b8b9fbc24532d719123b6a48bce
Jhonathan <jhonathan@Jhonathans-Mac-mini.local>
2022-12-08 14:07:48 -0300

    expenses CRUD routes added

--- backend/expenses/expenses.controller.js
+++ backend/expenses/expenses.controller.js
@@ -1,4 +1,5 @@
 import mongoClient from "../db.js";
+import {ObjectId} from "mongodb";
 
 const collection=mongoClient.collection('expenses')
 
@@ -10,4 +11,38 @@ export async function getAllExpenses(req, res){
         response
     })
 
+}
+
+export async function updateExpense(req, res){
+
+    const {expense} = req.body
+    const id = req.params.id
+
+    await collection.updateOne({_id: new ObjectId(id)}, {$set: {expense}})
+
+    res.status(200).send({
+        message: 'Your expense was updated.'
+    })
+}
+
+export async function createExpense(req, res){
+
+    const {expense} = req.body
+
+    await collection.insertOne(expense)
+
+    res.status(201).send({
+        message: 'Your expense was created.'
+    })
+}
+
+export async function deleteExpense(req, res){
+
+    const id = req.params.id
+
+    await collection.deleteOne({_id: new ObjectId(id)})
+
+    res.status(204).send({
+        message: 'Your expense was deleted.'
+    })
 }
--- backend/expenses/expenses.routes.js
+++ backend/expenses/expenses.routes.js
@@ -1,12 +1,13 @@
 import app from "../app.js";
 import {Router} from "express";
-import {getAllExpenses} from "./expenses.controller.js";
+import {createExpense, deleteExpense, getAllExpenses, updateExpense} from "./expenses.controller.js";
 
 //ROTAS CRUD
 
 const expensesRouter = Router()
 
-expensesRouter.route('/expenses').get(getAllExpenses)
+expensesRouter.route('/expenses').get(getAllExpenses).post(createExpense)
+expensesRouter.route('/expenses/:id').patch(updateExpense).delete(deleteExpense)
 
 
 
